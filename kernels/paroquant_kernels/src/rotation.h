1228c55bd28402db4247b01af1e61c39235c9737
Haisheng Chen <hac048@ucsd.edu>
2025-10-10 16:16:26 +0000

    temporary removal of kernels
    Signed-off-by: Haisheng Chen <hac048@ucsd.edu>

--- kernels/paroquant_kernels/src/rotation.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <torch/extension.h>
-
-torch::Tensor rotate_dynamic(at::Tensor x,
-                             at::Tensor idx,
-                             at::Tensor theta,
-                             c10::optional<at::Tensor> scales_opt);
