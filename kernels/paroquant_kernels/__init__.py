3f52e9c641af0ee945fcc4106ce6007fb64e178f
SubSir <tiancaizhangdaxian@sjtu.edu.cn>
2025-11-20 14:12:27 +0000

    fix: add missing group_size parameter to fake kernel registration

--- kernels/paroquant_kernels/__init__.py
+++ kernels/paroquant_kernels/__init__.py
@@ -5,7 +5,7 @@ from . import _C
 
 
 @torch.library.register_fake("rotation::rotate")
-def _fake_kernel(x, idx_ij, theta, scales):
+def _fake_kernel(x, idx_ij, theta, scales, group_size=128):
     return torch.empty_like(x)
 
 
